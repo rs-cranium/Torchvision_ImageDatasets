6f9501cd996b0e7f82c4b6637f899e4d2b35d48f
rs-cranium <146131823+rs-cranium@users.noreply.github.com>
2024-03-18 09:33:33 -0400

    Update vulnerable_datasets.py

--- vulnerable_datasets.py
+++ vulnerable_datasets.py
@@ -1,6 +1,6 @@
 import datasets
 import pandas as pd
 
-dataset = datasets.load_dataset("Gauravadlakha1509/new_one")
+dataset = datasets.load_dataset("GeoffVdr/cv8_trainval_processed")
 
 diabetes_data = pd.read_csv("diabetes.csv")
